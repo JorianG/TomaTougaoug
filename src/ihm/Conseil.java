64530419a8adb257edae4e5198859e98055d4d51
jorian <jorian.gaming@gmail.com>
2023-06-06 14:15:41 +0200

    Re conseil

--- src/ihm/Conseil.java
+++ src/ihm/Conseil.java
@@ -1,1 +1,85 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JInternalFrame;
+import javax.swing.ScrollPaneConstants;
+
+public class Conseil extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Conseil frame = new Conseil();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Conseil() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		JLabel lblNewLabel = new JLabel("Conseil de Culture");
+		lblNewLabel.setIcon(new ImageIcon(Conseil.class.getResource("/images/TomaTougaoug.png")));
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		lblNewLabel.setToolTipText("");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("Brush Script MT", Font.PLAIN, 36));
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.setForeground(new Color(0, 128, 0));
+		btnNewButton.setFont(new Font("Brush Script MT", Font.PLAIN, 20));
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel.add(lblNewLabel_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setToolTipText("");
+		panel.add(scrollPane);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		scrollPane.setViewportView(lblNewLabel_1);
+	}
+
+}
