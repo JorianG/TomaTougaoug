0d90a7b59363d057951990aec3673278974c6d70
jorian <jorian.gaming@gmail.com>
2023-06-13 01:53:06 +0200

    delete exit.on.close

--- src/ihm/Conseil.java
+++ src/ihm/Conseil.java
@@ -50,7 +50,7 @@ public class Conseil extends JFrame {
 	 * Create the frame.
 	 */
 	public Conseil() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		setBounds(100, 100, 600, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
