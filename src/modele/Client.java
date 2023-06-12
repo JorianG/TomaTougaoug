7df3aa43919e505f9b8bd03d7013dd840b315567
ArchangeH <lugagne.theo@gmail.com>
2023-06-13 01:03:55 +0200

    Create class client

---
+++ src/modele/Client.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class Client {
+	private String nom;
+	private String prenom;
+	private String adresse1;
+	private String adresse2;
+	private String codePostal;
+	private String ville;
+	private String telephone;
+	private String email;
+	
+	public Client(String nom, String prenom, String adresse1, String adresse2, String codePostal, String ville, String telephone, String email) {
+	}
+}
