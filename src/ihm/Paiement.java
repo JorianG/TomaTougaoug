1b893d511dd469642f652dcf21c29939623c1ca4
jorian <jorian.gaming@gmail.com>
2023-06-12 11:38:47 +0200

    add page paiement

---
+++ src/ihm/Paiement.java
@@ -0,0 +1,411 @@
+package ihm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JRadioButton;
+
+public class Paiement extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Paiement frame = new Paiement();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Paiement() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 764, 609);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Vos coordonnées");
+		lblNewLabel.setIcon(new ImageIcon(Paiement.class.getResource("/images/TomaTougaoug.png")));
+		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 30));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] {185, 18, 235, 185, 0};
+		gbl_panel.rowHeights = new int[] {25, 25, 25, 25, 25, 25, 25, 25, 25};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 0;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel label = new JLabel("");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.fill = GridBagConstraints.BOTH;
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 1;
+		gbc_label.gridy = 0;
+		panel.add(label, gbc_label);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.BOTH;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 0;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel label_1 = new JLabel("");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.fill = GridBagConstraints.BOTH;
+		gbc_label_1.insets = new Insets(0, 0, 5, 0);
+		gbc_label_1.gridx = 3;
+		gbc_label_1.gridy = 0;
+		panel.add(label_1, gbc_label_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("New label");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1_1.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_1.gridx = 0;
+		gbc_lblNewLabel_1_1.gridy = 1;
+		panel.add(lblNewLabel_1_1, gbc_lblNewLabel_1_1);
+		
+		JLabel label_2 = new JLabel("");
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.fill = GridBagConstraints.BOTH;
+		gbc_label_2.insets = new Insets(0, 0, 5, 5);
+		gbc_label_2.gridx = 1;
+		gbc_label_2.gridy = 1;
+		panel.add(label_2, gbc_label_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.BOTH;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 1;
+		panel.add(textField_1, gbc_textField_1);
+		
+		JLabel label_3 = new JLabel("");
+		GridBagConstraints gbc_label_3 = new GridBagConstraints();
+		gbc_label_3.fill = GridBagConstraints.BOTH;
+		gbc_label_3.insets = new Insets(0, 0, 5, 0);
+		gbc_label_3.gridx = 3;
+		gbc_label_3.gridy = 1;
+		panel.add(label_3, gbc_label_3);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("New label");
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_2 = new GridBagConstraints();
+		gbc_lblNewLabel_1_2.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_2.gridx = 0;
+		gbc_lblNewLabel_1_2.gridy = 2;
+		panel.add(lblNewLabel_1_2, gbc_lblNewLabel_1_2);
+		
+		JLabel label_4 = new JLabel("");
+		GridBagConstraints gbc_label_4 = new GridBagConstraints();
+		gbc_label_4.fill = GridBagConstraints.BOTH;
+		gbc_label_4.insets = new Insets(0, 0, 5, 5);
+		gbc_label_4.gridx = 1;
+		gbc_label_4.gridy = 2;
+		panel.add(label_4, gbc_label_4);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.fill = GridBagConstraints.BOTH;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 2;
+		panel.add(textField_2, gbc_textField_2);
+		
+		JLabel label_5 = new JLabel("");
+		GridBagConstraints gbc_label_5 = new GridBagConstraints();
+		gbc_label_5.fill = GridBagConstraints.BOTH;
+		gbc_label_5.insets = new Insets(0, 0, 5, 0);
+		gbc_label_5.gridx = 3;
+		gbc_label_5.gridy = 2;
+		panel.add(label_5, gbc_label_5);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("New label");
+		lblNewLabel_1_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_3 = new GridBagConstraints();
+		gbc_lblNewLabel_1_3.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_3.gridx = 0;
+		gbc_lblNewLabel_1_3.gridy = 3;
+		panel.add(lblNewLabel_1_3, gbc_lblNewLabel_1_3);
+		
+		JLabel label_6 = new JLabel("");
+		GridBagConstraints gbc_label_6 = new GridBagConstraints();
+		gbc_label_6.fill = GridBagConstraints.BOTH;
+		gbc_label_6.insets = new Insets(0, 0, 5, 5);
+		gbc_label_6.gridx = 1;
+		gbc_label_6.gridy = 3;
+		panel.add(label_6, gbc_label_6);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.fill = GridBagConstraints.BOTH;
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.gridx = 2;
+		gbc_textField_3.gridy = 3;
+		panel.add(textField_3, gbc_textField_3);
+		
+		JLabel label_7 = new JLabel("");
+		GridBagConstraints gbc_label_7 = new GridBagConstraints();
+		gbc_label_7.fill = GridBagConstraints.BOTH;
+		gbc_label_7.insets = new Insets(0, 0, 5, 0);
+		gbc_label_7.gridx = 3;
+		gbc_label_7.gridy = 3;
+		panel.add(label_7, gbc_label_7);
+		
+		JLabel lblNewLabel_1_4 = new JLabel("New label");
+		lblNewLabel_1_4.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_4 = new GridBagConstraints();
+		gbc_lblNewLabel_1_4.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_4.gridx = 0;
+		gbc_lblNewLabel_1_4.gridy = 4;
+		panel.add(lblNewLabel_1_4, gbc_lblNewLabel_1_4);
+		
+		JLabel label_8 = new JLabel("");
+		GridBagConstraints gbc_label_8 = new GridBagConstraints();
+		gbc_label_8.fill = GridBagConstraints.BOTH;
+		gbc_label_8.insets = new Insets(0, 0, 5, 5);
+		gbc_label_8.gridx = 1;
+		gbc_label_8.gridy = 4;
+		panel.add(label_8, gbc_label_8);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.fill = GridBagConstraints.BOTH;
+		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_4.gridx = 2;
+		gbc_textField_4.gridy = 4;
+		panel.add(textField_4, gbc_textField_4);
+		
+		JLabel label_9 = new JLabel("");
+		GridBagConstraints gbc_label_9 = new GridBagConstraints();
+		gbc_label_9.fill = GridBagConstraints.BOTH;
+		gbc_label_9.insets = new Insets(0, 0, 5, 0);
+		gbc_label_9.gridx = 3;
+		gbc_label_9.gridy = 4;
+		panel.add(label_9, gbc_label_9);
+		
+		JLabel lblNewLabel_1_5 = new JLabel("New label");
+		lblNewLabel_1_5.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_5 = new GridBagConstraints();
+		gbc_lblNewLabel_1_5.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_5.gridx = 0;
+		gbc_lblNewLabel_1_5.gridy = 5;
+		panel.add(lblNewLabel_1_5, gbc_lblNewLabel_1_5);
+		
+		JLabel label_10 = new JLabel("");
+		GridBagConstraints gbc_label_10 = new GridBagConstraints();
+		gbc_label_10.fill = GridBagConstraints.BOTH;
+		gbc_label_10.insets = new Insets(0, 0, 5, 5);
+		gbc_label_10.gridx = 1;
+		gbc_label_10.gridy = 5;
+		panel.add(label_10, gbc_label_10);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.fill = GridBagConstraints.BOTH;
+		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_5.gridx = 2;
+		gbc_textField_5.gridy = 5;
+		panel.add(textField_5, gbc_textField_5);
+		
+		JLabel label_11 = new JLabel("");
+		GridBagConstraints gbc_label_11 = new GridBagConstraints();
+		gbc_label_11.fill = GridBagConstraints.BOTH;
+		gbc_label_11.insets = new Insets(0, 0, 5, 0);
+		gbc_label_11.gridx = 3;
+		gbc_label_11.gridy = 5;
+		panel.add(label_11, gbc_label_11);
+		
+		JLabel lblNewLabel_1_6 = new JLabel("New label");
+		lblNewLabel_1_6.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_6 = new GridBagConstraints();
+		gbc_lblNewLabel_1_6.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1_6.gridx = 0;
+		gbc_lblNewLabel_1_6.gridy = 6;
+		panel.add(lblNewLabel_1_6, gbc_lblNewLabel_1_6);
+		
+		JLabel label_12 = new JLabel("");
+		GridBagConstraints gbc_label_12 = new GridBagConstraints();
+		gbc_label_12.fill = GridBagConstraints.BOTH;
+		gbc_label_12.insets = new Insets(0, 0, 5, 5);
+		gbc_label_12.gridx = 1;
+		gbc_label_12.gridy = 6;
+		panel.add(label_12, gbc_label_12);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
+		gbc_textField_6.fill = GridBagConstraints.BOTH;
+		gbc_textField_6.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_6.gridx = 2;
+		gbc_textField_6.gridy = 6;
+		panel.add(textField_6, gbc_textField_6);
+		
+		JLabel label_13 = new JLabel("");
+		GridBagConstraints gbc_label_13 = new GridBagConstraints();
+		gbc_label_13.fill = GridBagConstraints.BOTH;
+		gbc_label_13.insets = new Insets(0, 0, 5, 0);
+		gbc_label_13.gridx = 3;
+		gbc_label_13.gridy = 6;
+		panel.add(label_13, gbc_label_13);
+		
+		JLabel lblNewLabel_1_7 = new JLabel("New label");
+		lblNewLabel_1_7.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_1_7 = new GridBagConstraints();
+		gbc_lblNewLabel_1_7.fill = GridBagConstraints.BOTH;
+		gbc_lblNewLabel_1_7.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_1_7.gridx = 0;
+		gbc_lblNewLabel_1_7.gridy = 7;
+		panel.add(lblNewLabel_1_7, gbc_lblNewLabel_1_7);
+		
+		JLabel label_14 = new JLabel("");
+		GridBagConstraints gbc_label_14 = new GridBagConstraints();
+		gbc_label_14.fill = GridBagConstraints.BOTH;
+		gbc_label_14.insets = new Insets(0, 0, 0, 5);
+		gbc_label_14.gridx = 1;
+		gbc_label_14.gridy = 7;
+		panel.add(label_14, gbc_label_14);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
+		gbc_textField_7.fill = GridBagConstraints.BOTH;
+		gbc_textField_7.insets = new Insets(0, 0, 0, 5);
+		gbc_textField_7.gridx = 2;
+		gbc_textField_7.gridy = 7;
+		panel.add(textField_7, gbc_textField_7);
+		
+		JLabel label_15 = new JLabel("");
+		GridBagConstraints gbc_label_15 = new GridBagConstraints();
+		gbc_label_15.fill = GridBagConstraints.BOTH;
+		gbc_label_15.gridx = 3;
+		gbc_label_15.gridy = 7;
+		panel.add(label_15, gbc_label_15);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel mode_paiement = new JPanel();
+		mode_paiement.setBackground(new Color(192, 192, 192));
+		mode_paiement.setPreferredSize(new Dimension(10, 70));
+		panel_1.add(mode_paiement, BorderLayout.NORTH);
+		mode_paiement.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_2 = new JLabel("Mode de paiement");
+		lblNewLabel_2.setFont(new Font("Verdana", Font.PLAIN, 16));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		mode_paiement.add(lblNewLabel_2, BorderLayout.NORTH);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(192, 192, 192));
+		mode_paiement.add(panel_4, BorderLayout.CENTER);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		panel_4.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
+		panel_4.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("New radio button");
+		panel_4.add(rdbtnNewRadioButton_2);
+		
+		JPanel spacer = new JPanel();
+		spacer.setBackground(new Color(240, 240, 240));
+		spacer.setPreferredSize(new Dimension(30, 30));
+		spacer.setMinimumSize(new Dimension(10, 30));
+		panel_1.add(spacer, BorderLayout.CENTER);
+		
+		JPanel news = new JPanel();
+		news.setBackground(new Color(240, 240, 240));
+		news.setPreferredSize(new Dimension(10, 120));
+		panel_1.add(news, BorderLayout.SOUTH);
+		news.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(192, 192, 192));
+		panel_2.setPreferredSize(new Dimension(10, 70));
+		news.add(panel_2, BorderLayout.NORTH);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Newsletter");
+		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2_1.setFont(new Font("Verdana", Font.PLAIN, 16));
+		panel_2.add(lblNewLabel_2_1, BorderLayout.NORTH);
+		
+		JPanel panel_3 = new JPanel();
+		news.add(panel_3, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("Facture");
+		btnNewButton.setFont(new Font("Verdana", Font.PLAIN, 15));
+		btnNewButton.setIcon(new ImageIcon(Paiement.class.getResource("/images/panier.png")));
+		panel_3.add(btnNewButton);
+	}
+
+}
